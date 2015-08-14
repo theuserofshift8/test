ea43e7cb2631277f7793cca0758f973d6c964f01
theuserofshift8 <mailofwork@sina.cn>
2015-08-14 18:18:13 +0800

    test

---
+++ css/transform/Atest/aaaafiles/js728x90_preview.js
@@ -0,0 +1,6 @@
+document.writeln("<script type=\"text/javascript\">");
+document.writeln("    /*ÀÁÈËÍ¼¿â_´úÂë_728x90*/");
+document.writeln("    var cpro_id = \"u1787213\";");
+document.writeln("</script>");
+document.writeln("<script src=\"http://cpro.baidustatic.com/cpro/ui/c.js\" type=\"text/javascript\"></script>");
+document.writeln("");
---
+++ css/transform/Atest/aaaafiles/tj.js
@@ -0,0 +1,10 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?49542e19848a99b43a81376b929e6c72";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+
+window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"0","bdSize":"16"},"slide":{"type":"slide","bdImg":"2","bdPos":"right","bdTop":"125"}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];
