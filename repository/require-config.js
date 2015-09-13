25f40ce1e6d0c6b69101014f807f144e24660e02
EiJor <mailofwork@sina.cn>
2015-09-13 22:57:38 +0800

    some  message

--- repository/require-config.js
+++ repository/require-config.js
@@ -1,70 +1,11 @@
 /**
  * Created by bin on 2015/9/13.
  */
-/**
- * Created by randomnum on 2015/6/14.
- */
+
 require.config({
+    baseUrl: "/test/repository",
     paths: {
-        'domReady': 'domReady',
-        'jquery': 'jquery',
-        "mustache": "mustache",
-        "codemirror": "codemirror-compressed",
-        "codemirror/stex": "CodeMirror/stex",
-        "jquery.ui": "jquery-ui.min",
-        "jquery.form": "jquery.form",
-        "jquery.markitup": "markitup/jquery.markitup",
-        "jquery.leanModal": "jquery.leanModal.min",
-        "jquery.ajaxQueue": "jquery.ajaxQueue",
-        "jquery.smoothScroll": "jquery.smooth-scroll.min",
-        "jquery.timepicker": "timepicker/jquery.timepicker",
-        "jquery.cookie": "jquery.cookie",
-        "jquery.qtip": "jquery.qtip.min",
-        "jquery.scrollTo": "jquery.scrollTo-1.4.2-min",
-        "jquery.flot": "flot/jquery.flot.min",
-        "jquery.fileupload": "jQuery-File-Upload/js/jquery.fileupload",
-        "jquery.fileupload-process": "jQuery-File-Upload/js/jquery.fileupload-process",
-        "jquery.fileupload-validate": "jQuery-File-Upload/js/jquery.fileupload-validate",
-        "jquery.iframe-transport": "jQuery-File-Upload/js/jquery.iframe-transport",
-        "jquery.inputnumber": "html5-input-polyfills/number-polyfill",
-        "jquery.immediateDescendents": "coffee/src/jquery.immediateDescendents",
-        "datepair": "timepicker/datepair",
-        "date": "date",
-        "underscore": "underscore-min",
-        "underscore.string": "underscore.string.min",
-        "backbone": "backbone-min",
-        "backbone.associations": "backbone-associations-min",
-        "backbone.paginator": "backbone.paginator.min",
-        "tinymce": "tinymce/js/tinymce/tinymce.full.min",
-        "jquery.tinymce": "tinymce/js/tinymce/jquery.tinymce.min",
-        "xmodule": "/xmodule/xmodule",
-        "xblock/core": "js/xblock/core",
-        "xblock": "coffee/src/xblock",
-        "utility": "js/src/utility",
-        "accessibility": "js/src/accessibility_tools",
-        "draggabilly": "draggabilly.pkgd",
-        "URI": "URI.min",
-        "ieshim": "js/src/ie_shim",
-        "tooltip_manager": "js/src/tooltip_manager",
-        // Files needed for Annotations feature
-        "annotator": "ova/annotator-full",
-        "annotator-harvardx": "ova/annotator-full-firebase-auth",
-        "video.dev": "ova/video.dev",
-        "vjs.youtube": 'ova/vjs.youtube',
-        "rangeslider": 'ova/rangeslider',
-        "share-annotator": 'ova/share-annotator',
-        "richText-annotator": 'ova/richText-annotator',
-        "reply-annotator": 'ova/reply-annotator',
-        "grouping-annotator": 'ova/grouping-annotator',
-        "tags-annotator": 'ova/tags-annotator',
-        "diacritic-annotator": 'ova/diacritic-annotator',
-        "flagging-annotator": 'ova/flagging-annotator',
-        "jquery-Watch": 'ova/jquery-Watch',
-        "openseadragon": 'ova/openseadragon',
-        "osda": 'ova/OpenSeaDragonAnnotation',
-        "ova": 'ova/ova',
-        "catch": 'ova/catch/js/catch',
-        "handlebars": 'ova/catch/js/handlebars-1.1.2'
+        'jquery': 'jquery'
     }
 });
 
