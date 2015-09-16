362b62bda3acda1c64752dfbfc8a2a9d03664297
bin <27396781157@qq.com>
2015-09-17 04:34:25 +0800

    m

--- repository/require-config.js
+++ repository/require-config.js
@@ -9,6 +9,13 @@ require.config({
         domReady: 'domReady',
         backbone: 'backbone-min',
         underscore: 'underscore-min'
+    },
+
+    shim: {
+        backbone: {
+            deps: ['jquery','underscore'],
+            exports: 'Backbone'
+        }
     }
 });
 
