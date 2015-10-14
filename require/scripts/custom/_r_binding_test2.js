c306351775a15c9fc7fb32aa513c82c270ed625c
bin <2739811257@qq.com>
2015-10-14 22:00:19 +0800

    m

--- require/scripts/custom/_r_binding_test2.js
+++ require/scripts/custom/_r_binding_test2.js
@@ -2,12 +2,7 @@
  * Created by randomnum on 2015/9/9.
  */
 define(['jquery', 'domReady', 'datatable'], function ($, _domReady) {
-    _domReady(function () {
-        $('#bt1').on('click', function () {
-            $("#example22").DataTable();
-        })
-        $('#bt2').on('click', function () {
-            $("#example22").DataTable();
-        })
-    })
+
+    $("#example1").DataTable({});
+
 })
