03a7373fa83ba090bcaa5e28ad227cdd31674488
bin <2739811257@qq.com>
2015-11-15 12:09:23 +0800

    hrhr

--- README.md
+++
@@ -1,2 +0,0 @@
-# test
-poisonous    ,enjoy yourself.
--- require/scripts/custom/_r_binding_test2.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by randomnum on 2015/9/9.
- */
-define(['jquery', 'datatable'], function ($) {
-
-    $("#example1").DataTable({});
-
-})
